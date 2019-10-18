bbbfce556ad5ef605708b6db0d2a62c52d5170ee
stephen <stephen@morrissp.com>
2019-10-18 16:53:16 -0500

    final touches

--- public/script.js
+++ public/script.js
@@ -2,9 +2,9 @@ var LOAD_NUM = 4;
 var watcher;
 
 new Vue({
-	el:"#app",
+	el: "#app",
 	data: {
-		total: 0, 
+		total: 0,
 		products: [
 		],
 		cart: [],
@@ -14,7 +14,7 @@ new Vue({
 		results: []
 	},
 	methods: {
-		addToCart: function(product) {
+		addToCart: function (product) {
 			this.total += product.price;
 			var found = false;
 			for (var i = 0; i < this.cart.length; i++) {
@@ -23,65 +23,70 @@ new Vue({
 					found = true;
 				}
 			}
-			if (!found) {    
-				this.cart.push({ 
+			if (!found) {
+				this.cart.push({
 					id: product.id,
-					title: product.title, 
+					title: product.title,
 					price: product.price,
 					qty: 1
 				});
 			}
-		}, 
-		inc: function(item) {
+		},
+		inc: function (item) {
 			item.qty++;
 			this.total += item.price;
 		},
-		dec: function(item) {
+		dec: function (item) {
 			item.qty--;
 			this.total -= item.price;
 			if (item.qty <= 0) {
 				var i = this.cart.indexOf(item);
-				this.cart.splice(i,1);
+				this.cart.splice(i, 1);
 			}
 		},
-		onSubmit: function() {
-            this.products = [];
-            this.results = [];
+		onSubmit: function () {
+			this.products = [];
+			this.results = [];
 			this.loading = true;
 			var path = "/search?q=".concat(this.search);
 			this.$http.get(path)
-				.then(function(response) {
-					this.results = response.body;    
+				.then(function (response) {
+					this.results = response.body;
 					this.lastSearch = this.search;
 					this.appendResults();
 					this.loading = false;
 				});
 		},
-		appendResults: function() {
+		appendResults: function () {
 			if (this.products.length < this.results.length) {
-				var toAppend = this.results.slice(this.products.length,
-					LOAD_NUM+this.products.length);
+				var toAppend = this.results.slice(
+					this.products.length,
+					LOAD_NUM + this.products.length
+				);
+				this.products = this.products.concat(toAppend);
 			}
-            this.products = this.products.concat(toAppend);
 		}
 	},
 	filters: {
-		currency: function(price) {
+		currency: function (price) {
 			return "$".concat(price.toFixed(2));
 		}
-	}, 
-	created: function() {
+	},
+	created: function () {
 		this.onSubmit();
 	},
-	updated: function() {
+	updated: function () {
 		var sensor = document.querySelector("#product-list-bottom");
 		watcher = scrollMonitor.create(sensor);
 		watcher.enterViewport(this.appendResults);
 	},
-	beforeUpdate: function() {
+	beforeUpdate: function () {
 		if (watcher) {
 			watcher.destroy();
 			watcher = null;
-		}   }
+		}
+	}
 });
+
+
 
